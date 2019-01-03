5cbf2948f212054dd2c329803f3fd6ed3fa18a2b
Joshua Ahimaz <ahimaz.joshua@gmail.com>
2019-01-03 17:09:38 +0800

    Changed Database column value

--- database/migrations/2018_12_27_103746_create_listings_table.php
+++ database/migrations/2018_12_27_103746_create_listings_table.php
@@ -16,7 +16,7 @@ class CreateListingsTable extends Migration
         Schema::create('listings', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->string('desc');
+            $table->text('desc');
             $table->float('price')->default(0.00);
             $table->string('type');
             $table->string('location');
